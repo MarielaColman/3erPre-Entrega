ac6c3eed46d32228ce795d2459bff62bc5959c1b
MarielaColman <marielavcolmanv@gmail.com>
2023-06-11 21:34:11 -0300

    agregando js primer pre entrega

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nombre = prompt ("Ingrese tu nombre")
+let apellido = prompt (nombre + " " + "ingresa tu apellido")
+let nombreApellido = (nombre + " " + apellido)
+let edad = prompt (nombre + " " + "Ingrese tu edad")
+if (edad >= 18)[
+prompt ("Bienvenido a Ecofungi"+ " " + nombreApellido + ". " + "Ingresa tu usuario")]
+else [alert ("Debes ser mayor para ingresar a nuestra página")]
+
+let usuario = " "
+
+if (usuario != " "){
+    prompt ("Por favor ingresa tu contraseña")} 
+    else {prompt ("Ingresa tu usuario para continuar" + " " + nombre)}
