120a39ab5215da253b66d9379412c004e0298f93
MarielaColman <marielavcolmanv@gmail.com>
2023-07-03 20:50:37 -0300

    trabajando segunda pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
+
+class Cliente{
+    constructor(nombre, fechaNacimiento, sexo) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.id = this.crearIDaleatorio();
+    }
+    verDatos(){
+        return `id: ${this.id} 
+        Nombre: ${this.nombre}
+        Fecha de Nacimiento: ${this.fechaNacimiento.toLocaleDateString()}
+        Sexo: ${this.sexo}
+        Edad: ${this.calcularEdad()}`;
+    }
+
+    crearIDaleatorio() {
+        return Math.floor(Math.random() * 1000);
+    }
+    
+    calcularEdad(){
+        let hoy= new Date();
+        let edad= hoy.getFullYear() - this.fechaNacimiento.getFullYear();
+        let m= hoy.getMonth() - this.fechaNacimiento.getMonth();
+        if (m < 0 || (m === 0 && hoy.getDate() < this.fechaNacimiento.getDate())){
+            edad--;
+        }
+        return `${edad} años`;
+    }
+}
+
 let nombre = prompt ("Ingrese tu nombre")
-let apellido = prompt (nombre + " " + "ingresa tu apellido")
-let nombreApellido = (nombre + " " + apellido)
-let edad = prompt (nombre + " " + "Ingrese tu edad")
-if (edad >= 18)[
-prompt ("Bienvenido a Ecofungi"+ " " + nombreApellido + ". " + "Ingresa tu usuario")]
-else [alert ("Debes ser mayor para ingresar a nuestra página")]
+let fechaIngresada = prompt ("Coloque su fecha de nacimiento")
+let fechaNacimiento = new Date(fechaIngresada)
+let sexo = prompt ("Indique su sexo")
+console.log(fechaNacimiento);
+const cliente1 = new Cliente("Mariela",fechaNacimiento,"femenino");
+console.log(cliente1.verDatos());
 
-let usuario = " "
-
-if (usuario != " "){
-    prompt ("Por favor ingresa tu contraseña")} 
-    else {prompt ("Ingresa tu usuario para continuar" + " " + nombre)}
+ 
